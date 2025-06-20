f5f28a85466023604a1e6f4fb0a345b58944f66b
Douglas Gillespie <50671166+douggillespie@users.noreply.github.com>
2025-06-20 16:36:02 +0100

    Start JNA for Windows sound
    Aiming to bypass some of the limitations of Java sound.

---
+++ WindowsSoundJNA/WindowsSoundJNA.cpp
@@ -0,0 +1,234 @@
+// WindowsSoundJNA.cpp : Defines the exported functions for the DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "WindowsSoundJNA.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+#include "mmeapi.h"
+#include "mmdeviceapi.h"
+
+#pragma comment(lib, "WinMM.Lib")
+
+// This is an example of an exported variable
+//WINDOWSSOUNDJNA_API int nWindowsSoundJNA=0;
+
+// This is an example of an exported function.
+//WINDOWSSOUNDJNA_API int fnWindowsSoundJNA(void)
+//{
+//    return 0;
+//}
+
+// This is the constructor of a class that has been exported.
+//CWindowsSoundJNA::CWindowsSoundJNA()
+//{
+//    return;
+//}
+BOOL WINAPI DllMain(
+	HINSTANCE hinstDLL,  // handle to DLL module
+	DWORD fdwReason,     // reason for calling function
+	LPVOID lpReserved)  // reserved
+{
+	switch (fdwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		printf("In sound card DLL entry function reason %d\n", fdwReason);
+		//wEnumerateDevices();
+		fflush(stdout);
+		break;
+	}
+	return TRUE;
+}
+
+int nDevices = 0;
+WAVEINCAPS* deviceCaps = NULL;
+int* usedDevices = NULL;
+
+HWAVEIN hWaveIn;
+
+
+WINDOWSSOUNDJNA_API int enumerateDevices() {
+	int t = mixerGetNumDevs();
+	usedDevices = (int*) calloc(t, sizeof(int));
+	deviceCaps = (WAVEINCAPS*) calloc(t, sizeof(WAVEINCAPS));
+	nDevices = 0;
+	for (int i = 0; i < t; i++) {
+		WAVEINCAPS* caps = deviceCaps + nDevices;
+		int ret = waveInGetDevCaps(i, caps, sizeof(WAVEINCAPS));
+		// copy the name to a simple character array. 
+		char name[32];
+		for (int j = 0; j < 32; j++) {
+			name[j] = (char)caps->szPname[i];
+		}
+		name[31] = 0;
+		if (strlen(name)) {
+			nDevices++;
+		}
+	}
+	return nDevices;
+}
+WINDOWSSOUNDJNA_API int getNumDevices() {
+	return nDevices;
+}
+
+static char name[32];
+
+WINDOWSSOUNDJNA_API char* getDeviceName(int iDevice) {
+	if (iDevice >= nDevices) {
+		return NULL;
+	}
+	WAVEINCAPS* caps = deviceCaps + iDevice;
+	//char name[32];
+	for (int i = 0; i < 32; i++) {
+		name[i] = (char)caps->szPname[i];
+	}
+	name[31] = 0;
+	//memcpy(name, (char*)(caps.szPname), 32);
+	//printf(name);
+	//printf("\n");
+	//fflush(stdout);
+	return name;
+}
+
+
+WINDOWSSOUNDJNA_API int getDeviceFormats(int iDevice) {
+	if (iDevice >= nDevices) {
+		return -1;
+	}
+	return deviceCaps[iDevice].dwFormats;
+}
+
+WINDOWSSOUNDJNA_API int getDeviceChannels(int iDevice) {
+	if (iDevice >= nDevices) {
+		return -1;
+	}
+	return deviceCaps[iDevice].wChannels;
+}
+
+// https://learn.microsoft.com/en-us/previous-versions/dd743849(v=vs.85)
+void CALLBACK waveInProc(
+	HWAVEIN   hwi,
+	UINT      uMsg,
+	DWORD_PTR dwInstance,
+	DWORD_PTR dwParam1,
+	DWORD_PTR dwParam2
+) {
+	switch (uMsg) {
+	case WIM_OPEN:
+		printf("waveInProc OPEN : %d\n", uMsg);
+		break;
+	case WIM_CLOSE:
+		printf("waveInProc CLOSE : %d\n", uMsg);
+		break;
+	case WIM_DATA:
+		printf("waveInProc DATA : %d\n", uMsg);
+		break;
+	default:
+		printf("waveInProc other : %d\n", uMsg);
+		break;
+	}
+	fflush(stdout);
+}
+
+WINDOWSSOUNDJNA_API int waveStart(int iDevice, int nChannels, int sampleRate, int bitDepth) {
+	WAVEFORMATEX waveFormat;
+	waveFormat.cbSize = 0;
+	waveFormat.nChannels = nChannels;
+	waveFormat.nSamplesPerSec = sampleRate;
+	waveFormat.wBitsPerSample = bitDepth;
+	waveFormat.nBlockAlign = bitDepth / 8 * nChannels;
+	waveFormat.nAvgBytesPerSec = waveFormat.nBlockAlign * sampleRate;
+	waveFormat.wFormatTag = WAVE_FORMAT_PCM;
+
+	int res = waveInOpen(&hWaveIn, iDevice, &waveFormat, (DWORD_PTR) waveInProc, NULL, CALLBACK_FUNCTION);
+	res = waveInStart(hWaveIn);
+	return res;
+}
+
+WINDOWSSOUNDJNA_API int waveStop() {
+	if (hWaveIn == NULL) {
+		return 0;
+	}
+	waveInStop(hWaveIn);
+	waveInClose(hWaveIn);
+	hWaveIn = NULL;
+	return 0;
+}
+
+/*#define EXIT_ON_ERROR(hres)  \
+              if (FAILED(hres)) { return 0; }
+#define SAFE_RELEASE(punk)  \
+              if ((punk) != NULL)  \
+                { (punk)->Release(); (punk) = NULL; }
+
+const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
+const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+
+WINDOWSSOUNDJNA_API int wEnumerateDevices() {
+	HRESULT hr = S_OK;
+	IMMDeviceEnumerator *pEnumerator = NULL;
+	IMMDeviceCollection *pCollection = NULL;
+	IMMDevice *pEndpoint = NULL;
+	IPropertyStore *pProps = NULL;
+	LPWSTR pwszID = NULL;
+
+	hr = CoCreateInstance(
+		CLSID_MMDeviceEnumerator, NULL,
+		CLSCTX_ALL, IID_IMMDeviceEnumerator,
+		(void**)&pEnumerator);
+
+	EXIT_ON_ERROR(hr)
+
+		hr = pEnumerator->EnumAudioEndpoints(
+			eRender, DEVICE_STATE_ACTIVE,
+			&pCollection);
+	EXIT_ON_ERROR(hr)
+
+		UINT  count;
+	hr = pCollection->GetCount(&count);
+	EXIT_ON_ERROR(hr)
+
+		if (count == 0)
+		{
+			printf("No endpoints found.\n");
+		}
+	// Each loop prints the name of an endpoint device.
+	for (ULONG i = 0; i < count; i++)
+	{
+		// Get pointer to endpoint number i.
+		hr = pCollection->Item(i, &pEndpoint);
+		EXIT_ON_ERROR(hr)
+
+			// Get the endpoint ID string.
+			hr = pEndpoint->GetId(&pwszID);
+		EXIT_ON_ERROR(hr)
+
+			hr = pEndpoint->OpenPropertyStore(
+				STGM_READ, &pProps);
+		EXIT_ON_ERROR(hr)
+
+			PROPVARIANT varName;
+		// Initialize container for property value.
+		PropVariantInit(&varName);
+
+		// Get the endpoint's friendly-name property.
+		hr = pProps->GetValue(
+			PKEY_Device_FriendlyName, &varName);
+		EXIT_ON_ERROR(hr)
+
+			// Print endpoint friendly name and endpoint ID.
+			printf("Endpoint %d: \"%S\" (%S)\n",
+				i, varName.pwszVal, pwszID);
+
+		CoTaskMemFree(pwszID);
+		pwszID = NULL;
+		PropVariantClear(&varName);
+		SAFE_RELEASE(pProps)
+			SAFE_RELEASE(pEndpoint)
+	}
+	SAFE_RELEASE(pEnumerator)
+		SAFE_RELEASE(pCollection)
+		return;
+}*/
---
+++ WindowsSoundJNA/WindowsSoundJNA.h
@@ -0,0 +1,51 @@
+// The following ifdef block is the standard way of creating macros which make exporting
+// from a DLL simpler. All files within this DLL are compiled with the WINDOWSSOUNDJNA_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see
+// WINDOWSSOUNDJNA_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef WINDOWSSOUNDJNA_EXPORTS
+#define WINDOWSSOUNDJNA_API __declspec(dllexport)
+#else
+#define WINDOWSSOUNDJNA_API __declspec(dllimport)
+#endif
+
+// This class is exported from the dll
+/*class WINDOWSSOUNDJNA_API CWindowsSoundJNA {
+public:
+	CWindowsSoundJNA(void);
+	// TODO: add your methods here.
+};
+
+extern WINDOWSSOUNDJNA_API int nWindowsSoundJNA;
+
+WINDOWSSOUNDJNA_API int fnWindowsSoundJNA(void);*/
+
+extern "C" {
+	
+	WINDOWSSOUNDJNA_API int enumerateDevices();
+
+	WINDOWSSOUNDJNA_API int getNumDevices();
+
+	WINDOWSSOUNDJNA_API char* getDeviceName(int iDevice);
+
+	WINDOWSSOUNDJNA_API int getDeviceFormats(int iDevice);
+
+	WINDOWSSOUNDJNA_API int getDeviceChannels(int iDevice);
+
+	WINDOWSSOUNDJNA_API int waveStart(int iDevice, int nChannels, int sampleRate, int bitDepth);
+	
+	WINDOWSSOUNDJNA_API int waveStop();
+
+
+
+
+
+
+	// functions using the WASAPI instead of the older mm functions
+	// see https://learn.microsoft.com/en-us/windows/win32/multimedia/waveform-audio
+
+	//WINDOWSSOUNDJNA_API int wEnumerateDevices();
+
+
+}
